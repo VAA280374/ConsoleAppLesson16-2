658c44f8be3d8253dbb6a034a17e1af2da91336b
alexander.a.volkov@rambler.ru <alexander.a.volkov@rambler.ru>
2021-12-24 20:49:22 +0300

    Конечный результат

--- ConsoleAppLesson16-2/Program.cs
+++ ConsoleAppLesson16-2/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Text.Json.Serialization;
+using System.Text.Json;
 
 namespace ConsoleAppLesson16_2
 {
@@ -13,8 +15,41 @@ namespace ConsoleAppLesson16_2
         {
             string path = "Products.txt";
             string jsonProductsString;
-            using (StrimReader sr = )
+            using (StreamReader sr = new StreamReader(path))
+            { 
+                jsonProductsString = sr.ReadToEnd();
+                sr.Close();
+            }
+            // Console.WriteLine(jsonProductsString);
 
+            int count = 5;
+            int result = 0;
+            decimal _price = 0;
+            Product[] products = new Product[count];
+            products = JsonSerializer.Deserialize<Product[]>(jsonProductsString);
+            for (int i = 0; i < count; i++) 
+            {
+                if (products[i].Price > _price) 
+                { 
+                    result = i;
+                    _price = products[i].Price;
+                }
+                
+            }
+            Console.WriteLine("Самый дорогой товар : {0} ", products[result].Name);
+
+            Console.ReadKey();
         }
+    }
+    class Product
+    {
+        [JsonPropertyName("art")]
+        public int Art { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("price")]
+        public decimal Price { get; set; }
     }
 }
