365ed7f880740d67ca1a320302b8be8163095372
alexander.a.volkov@rambler.ru <alexander.a.volkov@rambler.ru>
2021-12-24 20:02:35 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppLesson16-2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleAppLesson16_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Products.txt";
+            string jsonProductsString;
+            using (StrimReader sr = )
+
+        }
+    }
+}
